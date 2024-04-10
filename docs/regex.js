f3ba9e1e0325ab04c937e86454d52846f583e616
onsinhhoc12lqd <166307835+onsinhhoc12lqd@users.noreply.github.com>
2024-04-10 17:48:34 +0700

    Add files via upload

--- docs/regex.js
+++ docs/regex.js
@@ -55,9 +55,15 @@ function assemble() {
 }
 
 function sh(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+    let currentIndex = array.length;
+
+    while (currentIndex != 0) {
+
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
     }
     return array;
 }
